de48ef6783652f5c321860988a629f0a577df6a8
Chesnova <myjobistester@gmail.com>
2024-10-18 21:51:13 +0200

    fix specifications

--- src/main/java/com/example/teamcity/api/spec/Specifications.java
+++ src/main/java/com/example/teamcity/api/spec/Specifications.java
@@ -15,22 +15,22 @@ public class Specifications {
         var requestBuilder = new RequestSpecBuilder();
         requestBuilder.addFilter(new RequestLoggingFilter());
         requestBuilder.addFilter(new ResponseLoggingFilter());
+        requestBuilder.setContentType(ContentType.JSON);
+        requestBuilder.setAccept(ContentType.JSON);
         return requestBuilder;
     }
 
     public static RequestSpecification superUserSpec(){
         var requestBuilder = reqBuilder();
-        requestBuilder.setBaseUri("http://%s:%s@%s".formatted("", Config.getProperty("superUserToken"), Config.getProperty("host")));
+        requestBuilder.setBaseUri("http://%s:%s@%s/httpAuth".formatted("", Config.getProperty("superUserToken"), Config.getProperty("host")));
         return requestBuilder.build();
     }
     public static RequestSpecification unauthSpec(){
         var requestBuilder = reqBuilder();
-        requestBuilder.setContentType(ContentType.JSON);
-        requestBuilder.setContentType(ContentType.JSON);
         return requestBuilder.build();
     }
 
-    public static RequestSpecification authSpec(User user){
+    public static RequestSpecification authSpec(User user) {
         var requestBuilder = reqBuilder();
         requestBuilder.setBaseUri("http://%s:%s@%s".formatted(user.getUsername(), user.getPassword(), Config.getProperty("host")));
         return requestBuilder.build();
